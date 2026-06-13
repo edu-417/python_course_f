30ccbc97832120dd5a5b68a727c8d6e16a8a46d3
Eduardo Meléndez <edgame127@gmail.com>
2026-06-13 17:33:08 -0500

    feat: added class_6

---
+++ main_externo.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("/home/eduardo/superprof/python_course/fernando/class_6")
+
+# print(sys.path)
+
+# import module
+# print(module.__file__)
+
+from module import isPrime
+
+
+
+print(isPrime(8))
+print(primes)
